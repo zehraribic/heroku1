a19f6cce53dc6a4fedb545984c48b991e0e7755f
zehraribic <72886506+zehraribic@users.noreply.github.com>
2021-03-18 17:40:06 +0100

    Update main.py

--- heroku1/main.py
+++ heroku1/main.py
@@ -1,10 +1,9 @@
-from flask import Flask, render_template, url_for
-import random
-from models import User, db
+from flask import Flask, render_template, 
+
 
 
 app = Flask(__name__)
-db.create_all()
+
 
 
 @app.route("/")
